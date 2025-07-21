6985c85227c3daad0f1f6e81e67b77f7471013ff
charlotte1227226 <andy20011019@gmail.com>
2025-07-21 23:55:19 +0800

    新增class4

---
+++ class/class_4/chrdevbase.c
@@ -0,0 +1,80 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/fs.h> // make 前沒有裝 看教學打得
+
+
+#define CHRDEVBASE_MAJOR       200              // 主設備號
+#define CHRDEVBASE_NAME        "chrdevbase"     //名字
+
+static int chrdevbase_open(struct inode *inode, struct file *filp)
+{
+    printk("chrdevbase_open\r\n");
+    return 0;
+}
+
+static int chrdevbase_release(struct inode *inode, struct file *filp)
+{
+    printk("chrdevbase_release\r\n");
+    return 0;
+}
+/*release就是close*/
+
+static ssize_t chrdevbase_read(struct file *filp, __user char *buf, size_t count, loff_t *ppos)
+{
+    printk("chrdevbase_read\r\n");
+    return 0;
+}
+
+static ssize_t chrdevbase_write(struct file *filp, const char __user *buf, size_t count, loff_t *ppos)
+{
+    printk("chrdevbase_write\r\n");
+    return 0;
+}
+
+static struct file_operations chrdevbase_fops = {
+    .owner = THIS_MODULE,
+    .open = chrdevbase_open,
+    .release = chrdevbase_release,
+    .read = chrdevbase_read,
+    .write = chrdevbase_write,
+};
+/*實現設備所需的函數*/
+
+/*
+字符設備操作合集
+*/
+
+
+static int __init chrdev_init(void)
+{
+    int ret = 0;
+    printk("chrdevbase_init\r\n");
+
+    /*註冊字符設備*/
+    ret = register_chrdev(CHRDEVBASE_MAJOR, CHRDEVBASE_NAME, &chrdevbase_fops);
+    if(ret < 0){
+        printk("chrdevbase init failed!\r\n");
+    }
+    return 0;
+}
+static void __exit chrdev_exit(void)
+{
+    printk("chrdevbese_exit\r\n");
+
+    /*註銷字符設備*/
+    unregister_chrdev(CHRDEVBASE_MAJOR, CHRDEVBASE_NAME);
+}
+
+/*
+模塊入口與出口
+*/
+
+module_init(chrdev_init);  //入口(加載模塊)
+module_exit(chrdev_exit);  //出口(卸載模塊)
+
+// 一定要加這行，不然 modpost 會報 missing MODULE_LICENSE()
+MODULE_LICENSE("GPL");
+// 以下兩行雖非必要，但建議都加上
+MODULE_AUTHOR("a0000");
+MODULE_DESCRIPTION("Simple Char Device Driver");
